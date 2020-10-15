7eef39babfb05270fdd73443dd3c5aabad04e566
Todd Garner <todd_garner@cable.comcast.com>
2020-10-14 23:19:06 -0400

    delving into styles

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,9 @@
 import React from "react"
-import { Link } from "gatsby"
+// import { Link } from "gatsby"
 import Header from "../components/header"
 
 export default function Home() {
-  return <div style={{ color: 'purple' }}>
-    <Link to="/contact/">Contact</Link>
+  return (<div>
     <Header headerText='Hello Gatsby!' />
-    <p>What a world</p>
-    <img src="https://source.unsplash.com/random/400x200" alt="" />
-  </div>
+  </div>)
 }
